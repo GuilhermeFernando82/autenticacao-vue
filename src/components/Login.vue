b675aad63a12a5689d01cdd800b1497ab2fc9818
unknown <guilherme.oliveira.zukk@agrotools.com.br>
2022-01-23 20:18:38 -0300

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="content">
+    <div class="container clearfix">
+    <h1>Login</h1>
+      <input type="text" class="form-control" v-model="Nome" placeholder="Nome"/><br>
+      <input type="text" class="form-control" v-model="Password" placeholder="Senha"/><br>
+      <button class="btn-success" @click="login">Login</button>
+     </div>
+  </div>
+</template>
+
+<script>
+export default({
+  name: 'Login',
+  props: {
+    token: {
+      type: String,
+      default: '',  
+    }
+  },
+  data: () => ({
+    Nome: '',
+    Password: '',
+    data: [],
+  }),
+  methods: {
+    login(){
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
+        "name": this.Nome,
+        "password": this.Password,
+        "role": "Nkkaiser"
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://localhost:7206/api/Usuario/Login", requestOptions)
+       .then(response => response.json())
+       .then(response => (this.data = response))
+       .then(response => {
+         if(response !== undefined){
+           console.log('data', this.data);
+           sessionStorage.setItem('@web/name', this.data[0].name);
+           sessionStorage.setItem('@web/password', this.data[0].password);
+           sessionStorage.setItem('@web/token', this.data[1]);
+          window.location.href="http://localhost:8080/register";
+       } else {
+         console.log('Usuário ou senha invalidos!!');
+       }
+       })
+       .catch(error => console.log('error', error));
+          }
+        }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  justify-content: center;
+  width: 500px;
+  height: 500px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+input {
+  margin: 5px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="content">
+    <div class="container clearfix">
+    <h1>Bem vindo {{ username }}</h1>
+    <h1>Cadastro de Usuários</h1>
+      <input type="text" class="form-control" v-model="Name" placeholder="Nome"/><br>
+      <input type="text" class="form-control" v-model="Password" placeholder="Senha"/><br>
+      <input type="text" class="form-control" v-model="Role" placeholder="Role"/><br>
+      <button class="btn-success" @click="register">Cadastrar</button>
+     </div>
+  </div>
+</template>
+
+<script>
+export default({
+  name: 'Register',
+  props: {
+    token: {
+      type: String,
+      default: '',
+    }
+  },
+  data: () => ({
+    username: sessionStorage.getItem('@web/name'),
+    Name: '',
+    Password: '',
+    Role: '',
+  }),
+  created(){
+    if (sessionStorage.getItem('@web/token') === null) {
+      window.location.href="http://localhost:8080/login";
+    }
+  },
+  methods: {
+    register(){
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", "Bearer "+sessionStorage.getItem('@web/token')+"");
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({
+        "name": this.Nome,
+        "password": this.Password,
+        "role": "Nkkaiser"
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+
+      fetch("https://localhost:7206/api/Usuario/authenticated", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+          }
+        }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  justify-content: center;
+  width: 500px;
+  height: 500px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+input {
+  margin: 5px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
